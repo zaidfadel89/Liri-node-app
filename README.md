a50bc875cedce03239b4289614f8b9ed6ec35041
Zaid Fadel <zaid.fadel89@gmail.com>
2019-10-02 00:57:58 -0500

    liri-node Homework is done

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# liri-node-app
+fff# liri-node-app
+**now we start with song API**
+`first i install npm file json by (npm init and npm i)`
+1-    `and i set environmental variables and requires( A- moment for timezone
+B- axios for API'S 
+ C- Spotify for song api 
+ D- fs for files system )`
+
+ 
+2- ``Create 2 variables Action and value 
+A- var action for the switch function
+B-  var value for API functions ``
+
+
+3- ``make song function
+and i grab the key from Keys.js
+with this code 
+var spotify = new Spotify(keys.spotify)``
+
+
+
+4- ``i start with Movie API 
+A- create var called movie and = to value
+B- start with axios git get APIs responses``
+
+
+5- ``start with concert APIs 
+A- create var called concert and = to value
+B- start with axios git get APIs responses``
+
+6- ``function total to grabing data from random.txt
+ with this codefs.readFile('random.txt', 'utf8', function(err, data) and split ``
+
--- liri.js
+++ liri.js
@@ -1,13 +1,22 @@
 //read ans set environmental variables
 require('dotenv').config();
+// require Keys.js
 var keys = require('./keys.js');
+// require moment (time zone)
+var moment = require('moment');
+// require api's
+
 var axios = require('axios');
+
 var Spotify = require('node-spotify-api');
+// file system
 var fs = require('fs');
 var action = process.argv[2];
 var value = process.argv[3];
+// now starting with Switch function
 switch (action) {
   case 'concert-this':
+    concert(value);
     break;
   case 'spotify-this-song':
     spotifySong(value);
@@ -17,11 +26,15 @@ switch (action) {
     fun(value);
     break;
   case 'do-what-it-says':
+    total(value);
+
     break;
   default:
     console.log('invaild enter');
 }
+// song api,s function
 function spotifySong(song) {
+  // Definition to exports.spotify inside keys.js
   var spotify = new Spotify(keys.spotify);
 
   spotify.search({ type: 'track', query: song }, function(err, data) {
@@ -41,25 +54,35 @@ function spotifySong(song) {
     }
   });
 }
+// now starting with movie Api
 function fun(value) {
   var movie = value;
-  var music = music;
 
   axios
     .get(' http://www.omdbapi.com/?t=' + movie + '&apikey=7d7e1950')
     .then(function(response) {
-      console.log('the Movie name is: ' + response.data.Title);
+      console.log('Title of the movie: ' + response.data.Title);
+      console.log('Year the movie came out: ' + response.data.Year);
+      console.log(
+        'Rotten Tomatoes Rating of the movie: ' + response.data.Rated
+      );
+      console.log('IMDB Rating of the movie: ' + response.data.imdbRating);
+      console.log(
+        'Country where the movie was produced: ' + response.data.Country
+      );
+      console.log('Language of the movie: ' + response.data.Language);
+      console.log('Plot of the movie: ' + response.data.Plot);
+      console.log('Actors in the movie: ' + response.data.Actors);
     })
 
+    // erorr function
     .catch(function(error) {
       if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log('---------------Data---------------');
+        console.log('--------------- Data ---------------');
         console.log(error.response.data);
-        console.log('---------------Status---------------');
+        console.log('--------------- Status ---------------');
         console.log(error.response.status);
-        console.log('---------------Status---------------');
+        console.log('--------------- Status ---------------');
         console.log(error.response.headers);
       } else if (error.request) {
         // The request was made but no response was received
@@ -71,4 +94,39 @@ function fun(value) {
       }
       console.log(error.config);
     });
+}
+// now starting with concert Api
+function concert(value) {
+  var concert = value;
+
+  axios
+    .get(
+      'https://rest.bandsintown.com/artists/' +
+        concert +
+        '?app_id=codingbootcamp'
+    )
+    // moment time function
+    .then(function(response) {
+      var time = moment(response.data.datatime).format('L');
+
+      console.log(response.data.name);
+      console.log(time);
+
+      console.log('loction:' + response.data.url);
+    });
+}
+
+// bring data from random.txt file
+
+function total() {
+  fs.readFile('random.txt', 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var deem = data.split(', ');
+
+    var actionOne = deem[0];
+    var action = deem[1];
+    console.log(deem);
+  });
 }
